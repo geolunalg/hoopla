342b42e2808c5938e5aa04aa4781189b1a3dc024
geolunalg <geovanni_luna@yahoo.com>
2025-10-23 16:22:44 +0000

    remove all stop words

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -2,6 +2,7 @@ import string
 from .search_utils import (
     DEFAULT_SEARCH_LIMIT,
     load_movies,
+    load_stop_word
 )
 
 
@@ -19,10 +20,15 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     return results
 
 
-def remove_punctuation(text):
+def remove_punctuation(text) -> str:
     translator = str.maketrans('', '', string.punctuation)
     return text.translate(translator)
 
 
-def tokenized_text(text):
-    return [val for val in text.split() if val != ""]
+def tokenized_text(text) -> list[str]:
+    tokens = []
+    stop_words = load_stop_word()
+    for val in text.split():
+        if val != "" and val not in stop_words:
+            tokens.append(val)
+    return tokens
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -4,10 +4,16 @@ import os
 DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+DATA_PATH = os.path.join(PROJECT_ROOT, "data")
 
 
 def load_movies() -> list[dict]:
-    with open(DATA_PATH, "r") as f:
+    with open(os.path.join(DATA_PATH, "movies.json"), "r") as f:
         data = json.load(f)
     return data["movies"]
+
+
+def load_stop_word() -> set:
+    with open(os.path.join(DATA_PATH, "stopwords.txt"), "r") as f:
+        data = f.read().splitlines()
+    return set(data)
