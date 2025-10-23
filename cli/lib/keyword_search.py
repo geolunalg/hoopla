ae4e8ca01b01688ef1172703fee5b3cc56b6ab25
geolunalg <geovanni_luna@yahoo.com>
2025-10-23 16:39:40 +0000

    reduce tokens to stem words

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -4,13 +4,14 @@ from .search_utils import (
     load_movies,
     load_stop_word
 )
+from nltk.stem import PorterStemmer
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
+    query_vals = set(tokenized_text(remove_punctuation(query.lower())))
     for movie in movies:
-        query_vals = set(tokenized_text(remove_punctuation(query.lower())))
         title_vals = tokenized_text(remove_punctuation(movie["title"].lower()))
         if any(value in " ".join(title_vals) for value in query_vals):
             results.append(movie)
@@ -28,7 +29,8 @@ def remove_punctuation(text) -> str:
 def tokenized_text(text) -> list[str]:
     tokens = []
     stop_words = load_stop_word()
+    stemmer = PorterStemmer()
     for val in text.split():
         if val != "" and val not in stop_words:
-            tokens.append(val)
+            tokens.append(stemmer.stem(val))
     return tokens
